953713ccb169718839166219d1c1509caa632777
Sarmat Khachirov <skhachirov12@gmail.com>
2020-02-27 13:57:33 +0300

    Init

---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app-bar color="grey darken-4" flat app>
+    <v-toolbar-title class="grey lighten-5"><h1 class="headline font-weight-medium">SendEther</h1></v-toolbar-title>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
