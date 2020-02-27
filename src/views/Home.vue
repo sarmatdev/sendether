3aaceaacf038058a357b27908243e90244704ab8
Sarmat Khachirov <skhachirov12@gmail.com>
2020-02-27 16:51:18 +0300

    Minor preparing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,28 @@
 <template>
   <div>
+    <Address />
   </div>
 </template>
 
 <script>
+import Address from './Address';
 export default {
-  components: {}
+  name: 'Home',
+  components: {
+    Address
+  },
+  data() {
+    return {};
+  },
+  methods: {}
 };
 </script>
+
+<style>
+.headline {
+  word-wrap: break-word;
+}
+.span {
+  background-color: aqua;
+}
+</style>
