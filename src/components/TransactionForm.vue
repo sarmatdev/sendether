e31169f771751c96c25696d308aa136b58ab2623
Sarmat Khachirov <skhachirov12@gmail.com>
2020-03-01 19:00:03 +0300

    Realize ethers sending functionality|works fine

--- src/components/TransactionForm.vue
+++ src/components/TransactionForm.vue
@@ -2,6 +2,7 @@
   <v-form>
     <v-layout column justify-center align-center>
       <v-card width="430px">
+        <v-card-title>Send Ethers Directly</v-card-title>
         <v-card-text>
           <v-text-field
             type="text"
@@ -9,15 +10,37 @@
             label="Wallet Address"
             v-model="tranaction.address"
           ></v-text-field>
-
           <v-text-field
             type="number"
             outlined
             min="0"
+            step="0.1"
             label="Amount"
             v-model="tranaction.amount"
           ></v-text-field>
-          Transacion fee is — {{ fee }} ETh
+          <!-- <v-card-subtitle class="pa-0">Choose Transaction speed</v-card-subtitle>
+              <v-radio-group v-model="speed">
+                <v-row>
+                  <v-col cols="12" xs="4" sm="4" md="4">
+                    <v-radio
+                      label="Fastest"
+                      color="info"
+                      value="21000"
+                    ></v-radio>
+                  </v-col>
+                  <v-col cols="12" xs="4" sm="4" md="4">
+                    <v-radio
+                      label="Fast"
+                      color="green"
+                      value="warning"
+                    ></v-radio>
+                  </v-col>
+                  <v-col cols="12" xs="4" sm="4" md="4">
+                    <v-radio label="Slow" color="error" value="error"></v-radio>
+                  </v-col>
+                </v-row>
+              </v-radio-group> -->
+          Transacion fee is {{ fee }} ETH
         </v-card-text>
         <v-card-text>
           <div class="subtitle-1">You also can customize your transaction!</div>
@@ -34,16 +57,16 @@
                 type="number"
                 outlined
                 min="0"
-                step="1000"
-                label="Gas Price"
-                v-model="tranaction.gasPrice"
+                label="Gas Limit"
+                v-model="tranaction.gasLimit"
               ></v-text-field>
               <v-text-field
                 type="number"
                 outlined
                 min="0"
-                label="Gas Limit"
-                v-model="tranaction.gasLimit"
+                step="1000"
+                label="Gas Price"
+                v-model="tranaction.gasPrice"
               ></v-text-field>
             </v-card-text>
           </div>
@@ -60,21 +83,22 @@
 export default {
   data: () => ({
     show: false,
+    // speed: '',
     tranaction: {
       address: '',
       amount: '',
-      gasPrice: 21000,
-      gasLimit: 1
+      gasPrice: 1,
+      gasLimit: 21000
     }
   }),
   methods: {
     sendTransaction() {
-      console.log(this.tranaction);
+      this.$store.dispatch('sendEther', this.tranaction)
     }
   },
   computed: {
     fee() {
-      return (this.tranaction.gasPrice * this.tranaction.gasLimit) / 1000000000;
+      return (this.tranaction.gasPrice * this.tranaction.gasLimit) / 100000000;
     }
   }
 };
--- src/store/modules/account.js
+++ src/store/modules/account.js
@@ -2,6 +2,7 @@ import Web3 from 'web3';
 const web3 = new Web3(
   'https://ropsten.infura.io/v3/9c85a82e358f46d389135967ceeeea82'
 );
+const Tx = require('ethereumjs-tx');
 
 export default {
   state: {
@@ -13,7 +14,7 @@ export default {
     }
   },
   actions: {
-    async createAccount({commit}) {
+    async createAccount({ commit }) {
       const account = await web3.eth.accounts.privateKeyToAccount(
         window.location.hash.substring(1)
       );
@@ -26,6 +27,37 @@ export default {
         )
       };
       commit('setAccount', data);
+    },
+    async sendEther({ commit, state }, transaction) {
+      web3.eth.getTransactionCount(state.account.address, (err, txCount) => {
+        // Build the transaction
+        const txObject = {
+          nonce: web3.utils.toHex(txCount),
+          to: transaction.address,
+          value: web3.utils.toHex(
+            web3.utils.toWei(`${transaction.amount}`, 'ether')
+          ),
+          gasLimit: web3.utils.toHex(transaction.gasLimit),
+          gasPrice: web3.utils.toHex(
+            web3.utils.toWei(`${transaction.gasPrice}`, 'gwei')
+          )
+        };
+        const privateKey = new Buffer(state.account.privateKey.slice(2, state.account.privateKey.length), 'hex')
+
+        // Sign the transaction
+        const tx = new Tx(txObject);
+        tx.sign(privateKey);
+
+        const serializedTx = tx.serialize();
+        const raw = '0x' + serializedTx.toString('hex');
+
+        // Broadcast the transaction
+        web3.eth.sendSignedTransaction(raw, (err, txHash) => {
+          console.log('txHash:', txHash);
+          console.log(err)
+          // Now go check etherscan to see the transaction!
+        });
+      });
     }
   },
   getters: {
