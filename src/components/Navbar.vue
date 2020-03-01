dbff13d0a8fd9ef27459d3db252bf8d826606293
Sarmat Khachirov <skhachirov12@gmail.com>
2020-03-01 18:58:49 +0300

    fix

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,6 @@
 <template>
   <v-app-bar color="orange darken-2" flat app>
     <v-toolbar-title class="grey lighten-5"><h1 class="headline font-weight-medium">SendEther</h1></v-toolbar-title>
-    <v-spacer></v-spacer>
-    <v-btn icon light @click.stop="drawer = !drawer">
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-
   </v-app-bar>
 </template>
 
