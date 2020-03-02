fe226dc01ec36d6f78aa83925cbea2acd671695c
Sarmat Khachirov <skhachirov12@gmail.com>
2020-03-02 20:25:42 +0300

    fix

--- src/views/Address.vue
+++ src/views/Address.vue
@@ -3,7 +3,6 @@
     <v-layout column justify-center align-center>
       <v-flex xs12 md10 class="text-center">
         <v-row align="baseline" justify="center">
-          <v-col cols="12"></v-col>
           <div class="headline pb-4">
             Your balance — {{ account.balance }} ETH
           </div>
@@ -17,12 +16,20 @@
         </div> -->
         <div v-if="link.show" class="headline pb-4">
           Explore transaction:
-          <a :href="`https://ropsten.etherscan.io/tx/${link}`" target="_blank"
+          <a
+            :href="`https://ropsten.etherscan.io/tx/${link.hash}`"
+            target="_blank"
             >https://ropsten.etherscan.io/tx/{{ link.hash }}</a
           >
         </div>
-        <div class="headline pb-4">Address: {{ account.address }}</div>
-        <div class="headline pb-4">Private Key: {{ account.privateKey }}</div>
+        <v-row>
+          <v-col cols="12">
+            <div class="headline pb-4">Address: {{ account.address }}</div>
+            <div class="headline pb-4">
+              Private Key: {{ account.privateKey }}
+            </div>
+          </v-col>
+        </v-row>
       </v-flex>
     </v-layout>
   </v-container>
