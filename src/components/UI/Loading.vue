3cad45f45f1553f54456b73be4680b956db9f6ba
Sarmat Khachirov <skhachirov12@gmail.com>
2020-03-02 15:41:39 +0300

    Create Loading spinner

---
+++ src/components/UI/Loading.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-row align="center" justify="center">
+    <v-row justify="center">
+      <v-overlay absolute>
+        <v-progress-circular indeterminate size="64"></v-progress-circular>
+      </v-overlay>
+    </v-row>
+  </v-row>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
