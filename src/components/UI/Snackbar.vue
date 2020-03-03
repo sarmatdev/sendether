88c44f2b70ad19a2a3f89eed3fa1efa655191b6c
Sarmat Khachirov <skhachirov12@gmail.com>
2020-03-03 08:29:29 +0300

    Create snackbar component| set snackbar for tx

---
+++ src/components/UI/Snackbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-snackbar
+    v-model="snackbar.show"
+    :timeout="timeout"
+    top
+    right
+    color="success"
+  >
+    {{ snackbar.text }}
+    <v-btn color="black" text dark @click="snackbar.show = false">
+      <v-icon>mdi-close</v-icon>
+    </v-btn>
+  </v-snackbar>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      timeout: 3000
+    };
+  },
+  computed: {
+    snackbar() {
+      return this.$store.getters.snackbar;
+    }
+  }
+};
+</script>
+
+<style></style>
--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -1,20 +1,35 @@
 export default {
   state: {
-    loading: false
+    loading: false,
+    snackbar: {
+      text: '',
+      show: false
+    }
   },
   mutations: {
     setLoading(state, payload) {
       state.loading = payload;
+    },
+    setSnackbar(state, payload) {
+      console.log(payload);
+      state.snackbar.show = payload.show;
+      state.snackbar.text = payload.text;
     }
   },
   actions: {
     setLoading({ commit }, loading) {
       commit('setLoading', loading);
+    },
+    setSnackbar({ commit }, loading) {
+      commit('setSnackbar', loading);
     }
   },
   getters: {
     loading(state) {
       return state.loading;
+    },
+    snackbar(state) {
+      return state.snackbar;
     }
   }
 };
