91946f8ff3e6faf89a34ba47b044888fb4673be9
Sarmat Khachirov <skhachirov12@gmail.com>
2020-03-05 14:14:38 +0300

    Setup vuelidate

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 import vuetify from './plugins/vuetify';
+import vuelidate from './plugins/vuelidate';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   vuetify,
+  vuelidate,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
---
+++ src/plugins/vuelidate.js
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+import Vuelidate from 'vuelidate';
+
+Vue.use(Vuelidate);
+
+export default Vuelidate;
