9fa64c286cd9cf592bc6a64ea2f2255fd785706f
Sarmat Khachirov <skhachirov12@gmail.com>
2020-03-05 16:43:32 +0300

    Implement copy to clipboard functionality

--- src/components/WalletData.vue
+++ src/components/WalletData.vue
@@ -2,14 +2,38 @@
   <v-row justify="start">
     <v-col cols="12">
       <div class="headline pb-4">Address: {{ account.address }}</div>
+      <v-btn color="success" @click="copyAddress(account.address)"
+        >Copy Address</v-btn
+      >
       <div class="headline pb-4">Private Key: {{ account.privateKey }}</div>
+      <v-btn color="success" @click="copyLink(account.privateKey)"
+        >Copy Private Key</v-btn
+      >
     </v-col>
   </v-row>
 </template>
 
 <script>
 export default {
-  props: ['account']
+  props: ['account'],
+  methods: {
+    copyAddress(address) {
+      this.$clipboard(address);
+      this.$store.commit('setSnackbar', {
+        text: 'Address copied!',
+        show: true,
+        color: 'success'
+      });
+    },
+    copyLink(key) {
+      this.$clipboard(key);
+      this.$store.commit('setSnackbar', {
+        text: 'Private Key copied!',
+        show: true,
+        color: 'success'
+      });
+    }
+  }
 };
 </script>
 
