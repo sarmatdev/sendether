bceac04b3f335dfd0a85e0446e8952827c885e4f
Sarmat Khachirov <skhachirov12@gmail.com>
2020-03-05 16:57:28 +0300

    Create Warning component

---
+++ src/components/Warning.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="caption pa-4">
+    Careful! The link above is a password! Only share it with the person who
+    will use the money. Don't trust this website? It's opensource and hosted by
+    GitHub Pages! There is no way for us to steal anything from you. Go check
+    out the <a target="_blank" href="https://github.com/sarmatdev/sendether">code</a>.
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
