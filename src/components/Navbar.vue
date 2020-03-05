ceb40a1ed37cd5b5e116803791c9052f905ed6e7
Sarmat Khachirov <skhachirov12@gmail.com>
2020-03-05 17:36:48 +0300

    add warning message

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,15 +1,18 @@
 <template>
   <v-app-bar color="blue-grey lighten-1" flat app>
-    <v-toolbar-title><h1 class="headline font-weight-medium">SendEther</h1></v-toolbar-title>
+    <v-toolbar-title>
+      <h1 class="headline font-weight-medium">SendEther</h1>
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-title>
+      This app working in Ropsten Testnet! Get testnet ethers here
+      <a target="_blank" href="https://faucet.ropsten.be/">faucet.ropsten.be</a>
+    </v-toolbar-title>
   </v-app-bar>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
