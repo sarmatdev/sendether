740c0480b18c9d9e85149a813d16d196c326d7e6
Sarmat Khachirov <skhachirov12@gmail.com>
2020-03-06 19:12:40 +0300

    Create footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-footer padless color="grey lighten-2"  elevation="15">
+    <v-col class="text-center" cols="12">
+      <strong>Created by <a href="https://t.me/sarmat" target="_blank">Sarmat</a> — inspired by <a href="https://sendcrypto.live/" target="_blank">SendCrypto</a>❤️</strong>
+    </v-col>
+  </v-footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
